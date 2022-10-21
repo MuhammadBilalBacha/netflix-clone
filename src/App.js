c52d85aeb59068c9195c24e3413dd113bdf25393
Bilal <bilal.se.iiui@gmail.com>
2022-10-21 04:05:31 -0700

    initial added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
+import Banner from "./Banner";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar />
+      <div className="app">
+        <Banner />
+        {/* My Netflex Clone */}
+        <Row
+          style={{ width: "15rem", height: "15rem" }}
+          title="POPULAR"
+          isLargerRow={true}
+          fetchUrl={requests.fetchNetflixOriginals}
+        />
+        <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+        <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
+        <Row title="ACTION MOVEIS" fetchUrl={requests.fetchActionMoveis} />
+        <Row title="COMEDY MOVEIS" fetchUrl={requests.fetchCommedyMoveis} />
+        <Row title="HORROR MOVEIS" fetchUrl={requests.fetchHorrorMoveis} />
+        <Row title="ROMANCE MOVEIS" fetchUrl={requests.fetchRomanceMoveis} />
+        <Row
+          title="DOCUMENTARIES"
+          fetchUrl={requests.fetchDocumentariesMoveis}
+        />
+      </div>
+    </Fragment>
   );
 }
 
